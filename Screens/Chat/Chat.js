d93c02327bb43a4aa348e9de887c606488a11511
HM Muneeb <hm.muneeb.u.r@gmail.com>
2022-05-29 02:29:41 +0500

    Chat module Completed!

--- Screens/Chat/Chat.js
+++ Screens/Chat/Chat.js
@@ -1,12 +1,115 @@
-import { Text, View, Image, Pressable } from "react-native";
+import {
+  Text,
+  View,
+  Image,
+  Pressable,
+  TextInput,
+  ScrollView,
+} from "react-native";
 import styles from "./Chat.style";
+import { useState, useRef } from "react";
 import Icon from "react-native-vector-icons/Ionicons";
 import IconE from "react-native-vector-icons/Entypo";
+import IconM from "react-native-vector-icons/MaterialCommunityIcons";
+import IconF from "react-native-vector-icons/Feather";
 
 export default function Chat({ navigation }) {
+  const scrollViewRef = useRef();
+  const [msg, setMsg] = useState("");
+  const [key, setKey] = useState("");
+  //1 is current user
+  const [MsgContent, setMsgContent] = useState([]);
   function handleGoBackBtn() {
     console.log("PRESSED GOBACK!!!");
     navigation.goBack();
+  }
+  function handleInputMessage(e) {
+    setMsg(e);
+    console.log(msg);
+  }
+  function sendMessage() {
+    if (msg != " " && msg != "" && msg != null && msg != undefined) {
+      //Pushes the input Message "msg" to state array MsgContent
+      setMsgContent((old) => [
+        ...old,
+        {
+          key: msg + Date.now() + "hi",
+          Avatar: "Defualt",
+          Content: msg,
+          User: 1,
+        },
+      ]);
+      setMsg("");
+      console.log("Message Sent!!!");
+      console.log(MsgContent);
+      autoReply();
+    }
+  }
+  function renderMessage(msg) {
+    if (msg.User == 1) {
+      return (
+        <View style={styles.sentMessageContainer}>
+          <View style={styles.sentMessageContentContainer}>
+            <Text style={styles.sentMessageText}>{msg.Content}</Text>
+          </View>
+          <Image source={require("./assets/avatar.png")} />
+        </View>
+      );
+    } else if (msg.User == 2) {
+      return (
+        <View style={styles.recievedMessageContainer}>
+          <Image source={require("./assets/avatar.png")} />
+          <View style={styles.messageContentContainer}>
+            <Text style={styles.recievedMessageText}>{msg.Content}</Text>
+          </View>
+        </View>
+      );
+    }
+  }
+  function autoReply() {
+    if (msg == "hi") {
+      setTimeout(() => {
+        console.log("Message Recieved!!!");
+        //Pushes the input Message "msg" to state array MsgContent
+        setMsgContent((old) => [
+          ...old,
+          {
+            key: msg + Date.now() + "bye",
+            Avatar: "Defualt",
+            Content: "hello",
+            User: 2,
+          },
+        ]);
+      }, 1000);
+    } else if (msg == "bye") {
+      setTimeout(() => {
+        console.log("Message Recieved!!!");
+        //Pushes the input Message "msg" to state array MsgContent
+        setMsgContent((old) => [
+          ...old,
+          {
+            key: msg + Date.now() + "bye",
+            Avatar: "Defualt",
+            Content: "bye",
+            User: 2,
+          },
+        ]);
+      }, 1000);
+    } else {
+      setTimeout(() => {
+        console.log("Message Recieved!!!");
+        //Pushes the input Message "msg" to state array MsgContent
+        setMsgContent((old) => [
+          ...old,
+          {
+            key: msg + Date.now() + "bye",
+            Avatar: "Defualt",
+            Content: "samjh nahi aya",
+            User: 2,
+          },
+        ]);
+      }, 1000);
+    }
   }
   return (
     <View style={styles.chat}>
@@ -31,6 +134,47 @@ export default function Chat({ navigation }) {
             </View>
           </View>
           <IconE name="dots-three-vertical" size={32} color="white" />
+        </View>
+      </View>
+
+      <ScrollView
+        style={styles.messagesContainer}
+        ref={scrollViewRef}
+        onContentSizeChange={() =>
+          scrollViewRef.current.scrollToEnd({ animated: true })
+        }
+      >
+        {MsgContent.map((msg) => (
+          <View key={msg.key + Date.now()}>{renderMessage(msg)}</View>
+        ))}
+      </ScrollView>
+
+      <View style={styles.messageInputActionsContainer}>
+        <View style={styles.actionContainer}>
+          <View style={styles.inputFieldView}>
+            <IconM
+              style={styles.keyBoardIcon}
+              name="keyboard-settings-outline"
+              size={32}
+              color="white"
+            />
+            <TextInput
+              placeholder="Type a Message..."
+              style={styles.messageInputField}
+              placeholderTextColor={"rgba(255,255,255,0.5)"}
+              autoCorrect={false}
+              multiline={true}
+              autoCapitalize="none"
+              autoFocus={false}
+              selectionColor="#FF9D43"
+              autoComplete="off"
+              onChangeText={handleInputMessage}
+              value={msg}
+            />
+          </View>
+          <Pressable onPress={sendMessage} style={styles.sendMessageBtn}>
+            <IconF name="send" size={32} color="#FF9D43" />
+          </Pressable>
         </View>
       </View>
     </View>
--- Screens/Chat/Chat.style.js
+++ Screens/Chat/Chat.style.js
@@ -1,7 +1,9 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-  chat: {},
+  chat: {
+    height: "100%",
+  },
   chatHeader: {
     width: "100%",
     paddingTop: "10%",
@@ -49,5 +51,90 @@ export default StyleSheet.create({
   lastActiveText: {
     color: "rgba(000,255,000,0.65)",
     fontSize: 12,
+  },
+  messagesContainer: {
+    width: "100%",
+    paddingHorizontal: "5%",
+    marginBottom: 75,
+  },
+  //Recieved Message
+  recievedMessageContainer: {
+    minWidth: 30,
+    maxWidth: 270,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    marginVertical: 10,
+  },
+  messageContentContainer: {
+    marginLeft: 10,
+    padding: 10,
+    backgroundColor: "rgba(255,255,255,0.15)",
+    borderBottomRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  recievedMessageText: { color: "white" },
+
+  //Sent Message
+  sentMessageContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    marginVertical: 10,
+  },
+  sentMessageContentContainer: {
+    minWidth: 30,
+    maxWidth: 270,
+    marginRight: 10,
+    padding: 10,
+    backgroundColor: "#FF9D43",
+    borderBottomRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderTopLeftRadius: 10,
+  },
+  sentMessageText: { color: "white" },
+
+  // Input Actions
+  messageInputActionsContainer: {
+    minHeight: 75,
+    maxHeight: 100,
+    paddingHorizontal: "5%",
+    position: "absolute",
+    bottom: 0,
+    backgroundColor: "#1E1E1E",
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 10,
+  },
+  actionContainer: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  keyBoardIcon: {
+    marginHorizontal: 10,
+  },
+  inputFieldView: {
+    backgroundColor: "#292929",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    minHeight: 50,
+    paddingVertical: 5,
+    flex: 1,
+    borderRadius: 5,
+  },
+  messageInputField: {
+    color: "white",
+    width: "80%",
+  },
+  sendMessageBtn: {
+    marginLeft: 10,
   },
 });
