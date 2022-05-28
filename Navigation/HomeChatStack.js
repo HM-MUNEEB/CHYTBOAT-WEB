b7e41d79547af2b259705c06455be16e80c335ea
HM Muneeb <hm.muneeb.u.r@gmail.com>
2022-05-29 00:23:53 +0500

    Adjusted Stack Nav Animation

--- Navigation/HomeChatStack.js
+++ Navigation/HomeChatStack.js
@@ -5,9 +5,28 @@ import Chat from "../Screens/Chat/Chat";
 const Stack = createStackNavigator();
 
 export default function HomeChatStack() {
+  const config = {
+    animation: "spring",
+    config: {
+      stiffness: 5000,
+      damping: 500,
+      mass: 5,
+      overshootClamping: true,
+      restDisplacementThreshold: 0.01,
+      restSpeedThreshold: 0.01,
+    },
+  };
   return (
     <Stack.Navigator
-      screenOptions={{ headerShown: false, animationTypeForReplace: "push" }}
+      screenOptions={{
+        headerShown: false,
+        animationEnabled: true,
+        animationTypeForReplace: "push",
+        transitionSpec: {
+          open: config,
+          close: config,
+        },
+      }}
     >
       <Stack.Screen name="Navbar" component={Navbar} />
       <Stack.Screen name="Chat" component={Chat} />
